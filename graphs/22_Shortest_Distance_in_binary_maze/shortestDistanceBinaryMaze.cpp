282443b83c2e5d858e59909b12774bb37412fad2
ritopravosarker <ritopravosarker@gmail.com>
2023-05-10 13:01:35 +0530

    day6

--- graphs/22_Shortest_Distance_in_binary_maze/shortestDistanceBinaryMaze.cpp
+++ graphs/22_Shortest_Distance_in_binary_maze/shortestDistanceBinaryMaze.cpp
@@ -7,15 +7,43 @@ void graphInput (vector<int> [], int );
 
 int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int> destination) {
     int m = grid.size(), n = grid[0].size();
-    for(int i = 0; i<m; i++){
-        for(int j = 0; j<n; j++){
-            cout << grid[i][j] <<" ";
+    // for(int i = 0; i<m; i++){
+    //     for(int j = 0; j<n; j++){
+    //         cout << grid[i][j] <<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<source.first<<" "<<source.second<<endl;
+    // cout<<destination.first<<" "<<destination.second<<endl;
+    queue< pair<int, int> > q;
+    vector<vector<int>> dist(m, vector<int>(n, -1));
+    q.push(source);
+    dist[source.first][source.second] = 0;
+
+    int offset [] = {-1,0,1,0,-1};
+
+    while(!q.empty()){
+        int node_x = q.front().first;
+        int node_y = q.front().second;
+        q.pop();
+        for(int k =0; k<4; k++){
+            int x = node_x + offset[k];
+            int y = node_y + offset[k+1];
+            if(x>=0 && y>=0 && x<m && y<n && grid[x][y] == 1 && dist[x][y]==-1){
+                dist[x][y] = dist[node_x][node_y] + 1;
+                q.push({x, y});
+            }
         }
-        cout<<endl;
     }
-    cout<<source.first<<" "<<source.second<<endl;
-    cout<<destination.first<<" "<<destination.second<<endl;
-    return 0;
+
+    // for(int i = 0; i<m; i++){
+    //     for(int j = 0; j<n; j++){
+    //         cout<<dist[i][j]<< " ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    return dist[destination.first][destination.second];
 }
 
 
